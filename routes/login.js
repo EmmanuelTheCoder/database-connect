9ded39eb2b68d1a85086ee09b760e96682139318
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2022-03-07 16:56:28 +0100

    solving json

--- routes/login.js
+++ routes/login.js
@@ -8,20 +8,19 @@ login.use(express.json())
 login.use(express.urlencoded({extended: false}))
 
 login.post("/", async (req, res) =>{
-    const token_secret = "a23232fjeffkdfk344343kvddfd334"
+    
     const username = await User.findOne({username: req.body.username})
     const password = await User.findOne({password: req.body.password})
-
+    
     if(!username || !password){
         return res.send({message: "failed login", details: "username or password is not valid"})
     }
-
-    console.log(username._id)
-    //res.send("login successful");
-
-    const token = jwt.sign({_id: username._id}, `${process.env.SECRET_PASS}`);
-
+    
+    
+    const token = jwt.sign({email: username.email, username: username.username}, `${process.env.SECRET_PASS}`);
+    
     res.header("auth-token", token).send(token);  
+    //console.log(req.user);
 });
 
 module.exports = login
