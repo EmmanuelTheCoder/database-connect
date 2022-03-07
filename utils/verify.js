57bc16a372ef7fe33b11ae33cb92ba75d928c2e9
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2022-03-07 23:16:33 +0100

    verify middleware

--- utils/verify.js
+++ utils/verify.js
@@ -1,7 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) =>{
+    const token = req.header("auth-token")
 
+    if (!token){
+        return res.status(401).send("failed to authorize! Please proceed to login or signup")
+    }
+
+    try{
+        const verified = jwt.verify(token, `${process.env.SECRET_PASS}`);
+        req.user = verified
+    }catch(err){
+        res.status(400).json({message: "an error occured", err})
+    }
 }
 
 //module.exports = Verify
