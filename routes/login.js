1cee9e77af895fc1506317ce51a55bbb4f02c147
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2022-03-07 16:24:06 +0100

    jw token create on login

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const User = require("../utils/User");
 const jwt = require("jsonwebtoken");
+const { token } = require("morgan");
 
 const login = express.Router();
 login.use(express.json())
 login.use(express.urlencoded({extended: false}))
 
 login.post("/", async (req, res) =>{
+    const token_secret = "a23232fjeffkdfk344343kvddfd334"
     const username = await User.findOne({username: req.body.username})
     const password = await User.findOne({password: req.body.password})
 
@@ -15,10 +17,11 @@ login.post("/", async (req, res) =>{
     }
 
     console.log(username._id)
-    res.send("login successful");
+    //res.send("login successful");
 
-    
-      
+    const token = jwt.sign({_id: username._id}, `${process.env.SECRET_PASS}`);
+
+    res.header("auth-token", token).send(token);  
 });
 
 module.exports = login
