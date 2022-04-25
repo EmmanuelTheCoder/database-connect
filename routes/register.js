3f7e773f77b2631041c0c149fb5ff7abe1cb6db1
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2022-04-25 12:00:31 +0100

    revamped db

--- routes/register.js
+++ routes/register.js
@@ -11,22 +11,26 @@ register.post("/", async (req, res) =>{
     const signUp = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: req.body.password,
+        post: req.body.post
     })
 
     try{
-        const checkIfUserExists = User.findOne({email: req.body.email})
-        if(!checkIfUserExists){
             const user = await signUp.save()
             return res.status(200).json({message: "success", detail: user})
-        }
-        else{
-            res.status(400).send("This email exists. Please proceed to login or recover password")
-        }
+    
     }
     catch(err){
         res.status(401).json({message: "failed", error: err})
     }
 })
+
+register.post("/find", async (req, res) =>{
+    const username = await User.findOne({username: req.body.username});
+
+    const updated = await User.updateOne({username: req.body.username}, {$addToSet: {post: req.body.post}})
+
+    res.send(updated)
+});
 
 module.exports = register
--- utils/User.js
+++ utils/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+const postSchema = new mongoose.Schema({post: String})
 const User = new mongoose.Schema({
     username: {
         type: String,
@@ -9,11 +10,16 @@ const User = new mongoose.Schema({
         type: String,
         required: true
     },
+    post: {
+        type: Array
+        
+    },
     password: {
         type: String,
         required: true,
         min: 8
     },
+    
     date:{
         type: Date,
         default: Date.now()
